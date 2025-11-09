a862dd6ec403043ba8cd98ee30d9f8c8f77421d6
carlosstack <paytocarlos@gmail.com>
2025-11-09 13:53:29 -0400

    fix: Disk logic and simulation

--- Proyecto2-SO/src/main/java/main/classes/Disk.java
+++ Proyecto2-SO/src/main/java/main/classes/Disk.java
@@ -4,10 +4,91 @@
  */
 package main.classes;
 
+import java.awt.Color;
+
 /**
  *
  * @author payto
  */
-public class Disk { 
+public class Disk {
+    private int[] allocationTable; //Identificador de disponibilidad del bloque y su tabla de asignacion
+
+    private Process[] blockOwners; //asignar color en posicion correspondiente
+
+    private int totalBlocks;
+
+    public Disk(int totalBlocks) {
+        this.totalBlocks = totalBlocks;
+        this.allocationTable = new int[totalBlocks];
+        this.blockOwners = new Process[totalBlocks];
+
+        for (int i = 0; i < totalBlocks; i++) {
+            this.allocationTable[i] = 0; // 0 = FREE
+            this.blockOwners[i] = null;
+        }
+    }
     
+    
+    //Asignar bloques 
+    public int assignBlocks(Process owner, int blocksNeeded) {
+        if (blocksNeeded <= 0) {
+            return -1;
+        }
+
+        int firstBlock = -1;
+        int previousBlock = -1;
+        int blocksFound = 0;
+
+        for (int i = 0; i < this.totalBlocks; i++) {
+            if (allocationTable[i] == 0) {
+                blocksFound++;
+
+                allocationTable[i] = -1;
+                blockOwners[i] = owner;
+
+                if (firstBlock == -1) {
+                    firstBlock = i;
+                } else {
+                    allocationTable[previousBlock] = i;
+                }
+
+                previousBlock = i;
+
+                if (blocksFound == blocksNeeded) {
+                    System.out.println("Disk: Asignsdo" + blocksNeeded + " bloques para " + owner.getProcessName() + ". Primer bloque: " + firstBlock);
+                    return firstBlock; // Success!
+                }
+            }
+        }
+        System.err.println("Disk: ¡Espacio insuficiente! Error al asignar" + blocksNeeded + " bloques.");
+        return -1;
+    }
+
+    //Liberar bloques 
+    public void freeBlocks(int startBlock) {
+        int currentBlock = startBlock;
+        int nextBlock;
+
+        while (currentBlock != -1 && currentBlock < totalBlocks) {
+            nextBlock = allocationTable[currentBlock];
+
+            allocationTable[currentBlock] = 0;
+            blockOwners[currentBlock] = null;
+
+            currentBlock = nextBlock;
+        }
+        System.out.println("Disk: Cadena de bloques liberada a partir de " + startBlock);
+    }
+
+    // --- GETTERS (for the GUI) ---
+    public int getTotalBlocks() {
+        return totalBlocks;
+    }
+
+    public Color getColorForBlock(int index) {
+        if (index < 0 || index >= totalBlocks || blockOwners[index] == null) {
+            return Color.LIGHT_GRAY; // Color for "FREE"
+        }
+        return blockOwners[index].getColor();
+    }
 }
--- Proyecto2-SO/src/main/java/main/classes/Simulator.java
+++ Proyecto2-SO/src/main/java/main/classes/Simulator.java
@@ -15,12 +15,44 @@ import helpers.MyList;
 public class Simulator {
 
     //Lista para procesos
-    private final MyList<Process> MasterProcessList;
+    private final MyList<Process> masterProcessList;
     // Cola para E/S a disco
-    private final CustomQueue<IoRequest> DiskRequests;
+    private final CustomQueue<IoRequest> diskRequest;
+
+    private final Scheduler scheduler;
+    private Disk disk;
+    //17*17= 289 bloques de preferencia
 
     public Simulator() {
-        this.MasterProcessList = new MyList<>();
-        this.DiskRequests = new CustomQueue<>();
+        this.masterProcessList = new MyList<>();
+        this.diskRequest = new CustomQueue<>();
+        this.scheduler = new Scheduler();
+
+        this.disk = new Disk(289); //289 bloques en disco
     }
+
+    public void requestCreateFile(String path, int blockCount, String username) {
+
+        String processName = username + " (Create " + path + ")";
+        Process p = new Process(processName);
+        p.setState(Process.ProcessState.READY);
+
+        this.masterProcessList.add(p);
+
+        IoRequest request = new IoRequest(
+                p,
+                IoRequest.OperationType.CREATE_FILE,
+                path,
+                blockCount
+        );
+
+        this.diskRequest.enqueue(request);
+
+        updateGUI();
+    }
+
+    public void updateGUI() {
+
+    }
+
 }
